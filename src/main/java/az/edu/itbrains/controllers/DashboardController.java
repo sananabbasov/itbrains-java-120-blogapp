cacc8e28699e05697fcc998ed205de9b8b84b2b7
sananabbasov <sanan_abbasov@list.ru>
2024-05-14 17:10:33 +0400

    admin dashboard index is ready

---
+++ src/main/java/az/edu/itbrains/controllers/DashboardController.java
@@ -0,0 +1,15 @@
+package az.edu.itbrains.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+
+    @GetMapping("/admin")
+    public String index()
+    {
+        return  "admin/index";
+    }
+}
